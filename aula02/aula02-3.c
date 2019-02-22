fbbd6d4b8af323f520f30418115d2a6840b76e84
Danilo Abreu <daniloabreu1@gmail.com>
2019-02-22 10:58:58 -0300

    QUEST

--- aula02/aula02-3.c
+++ aula02/aula02-3.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 main(){
     system("color F1");
-    printf("DDDDDDDD\n");
-    printf("D      D\n");
-    printf("D      D\n");
-    printf("D      D\n");
-    printf("D      D\n");
-    printf(" D    D\n");
-    printf("  DDDD\n\n");
-    printf("AAAAAA   \n");
-    printf("   A  A\n");
-    printf("   A   A\n");
-    printf("   A   A\n");
-    printf("   A   A\n");
-    printf("   A  A\n");
-    printf("AAAAAA\n\n");
-    printf("S  SSSS\n");
-    printf("S  S  S\n");
-    printf("S  S  S\n");
-    printf("S  S  S\n");
-    printf("S  S  S\n");
-    printf("S  S  S\n");
-    printf("SSSS  S\n\n");
+    printf("********      ***       *         *\n");
+    printf("*      *     *   *     ***       * *\n");
+    printf("*      *    *     *   *****     *   *\n");
+    printf("*      *    *     *     *      *     *\n");
+    printf("*      *    *     *     *     *       *\n");
+    printf("*      *    *     *     *      *     *\n");
+    printf("*      *    *     *     *       *   *\n");
+    printf("*      *     *   *      *        * *\n");
+    printf("********      ***       *         *\n");
 }
